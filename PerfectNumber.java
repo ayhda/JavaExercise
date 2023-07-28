9e7edc3ba0301d479de64f85a0c87e4bbe976c90
ayhdaaa <88218769+ayhda@users.noreply.github.com>
2023-07-29 01:04:33 +0800

    Update PerfectNumber.java

--- PerfectNumber.java
+++ PerfectNumber.java
@@ -27,4 +27,19 @@ public class PerfectNumber {
             return false;
         }
     }
+
+    //Another awesome solution
+    /*
+    public static boolean isPerfectNumber(int number) {
+ 
+    int sumOfProperDivisors = 0;
+    int increment = (number % 2 == 0) ? 1 : 2;
+ 
+    for (int i = 1; i <= number / 2; i += increment)
+        if (number % i == 0)
+            sumOfProperDivisors += i;
+ 
+    return sumOfProperDivisors == number && number > 1;
+    }
+    */
 }
